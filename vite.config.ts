5a854871d2ec52129de7d8a720d381d3842b1e92
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-07-05 14:26:02 +0200

    changed base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import griffel from "@griffel/vite-plugin";
 
 export default defineConfig(({ command }) => ({
+  base: "/modules",
   plugins: [
     react(),
     command === "build" && griffel()
