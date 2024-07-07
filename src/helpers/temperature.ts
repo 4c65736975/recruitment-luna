3f87f7c7fc330f148bf2d9baba9f9d0f7e674dad
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-07-07 17:52:15 +0200

    initial commit

---
+++ src/helpers/temperature.ts
@@ -0,0 +1,18 @@
+/**
+ * temperature.ts
+ *
+ * Copyright (c) 2024 Damian Leśniewski. All Rights Reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const getIsCurrentTempInRange = (current: number, target: number): boolean => {
+  const diff = Math.abs(target - current);
+
+  return diff < 0.5;
+};
+
+export const toCelsius = (value: number): string => {
+  return value.toFixed(2) + "°C";
+};
