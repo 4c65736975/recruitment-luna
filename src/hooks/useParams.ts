e4e604a10599ff32b924a27cff9c973f52624e70
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-07-11 15:31:35 +0200

    initial commit

---
+++ src/hooks/useParams.ts
@@ -0,0 +1,17 @@
+/**
+ * useParams.ts
+ *
+ * Copyright (c) 2024 Damian Leśniewski. All Rights Reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { useParams as useRouterParams } from "react-router-dom";
+
+
+/**
+ * Override useParams to get rid of TypeScript complaints about possibly undefined values.
+ * Since it's a route parameter (not a query string), if the parameter doesn't exist, it means the route is different.
+ */
+export const useParams = <T extends Record<string, unknown>>() => useRouterParams() as T;
