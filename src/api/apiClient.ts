778c78473bfb1ecbb258976fdbc327d2c8043152
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-07-11 15:31:59 +0200

    initial commit

---
+++ src/api/apiClient.ts
@@ -0,0 +1,17 @@
+/**
+ * apiClient.ts
+ *
+ * Copyright (c) 2024 Damian Leśniewski. All Rights Reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Axios from "axios";
+
+export const api = Axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
